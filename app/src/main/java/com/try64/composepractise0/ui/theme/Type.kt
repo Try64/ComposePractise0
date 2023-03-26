a0df536dfe8f56aa733854519c0f7431b6949292
Abu Zafar Newton <newton@shurjomukhi.com.bd>
2023-03-26 14:11:16 +0600

    added custom designs for font and style

--- app/src/main/java/com/try64/composepractise0/ui/theme/Type.kt
+++ app/src/main/java/com/try64/composepractise0/ui/theme/Type.kt
@@ -1,19 +1,42 @@
 package com.try64.composepractise0.ui.theme
 
 import androidx.compose.material.Typography
+import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
+import com.try64.composepractise0.R
+
+@OptIn(ExperimentalTextApi::class)
+val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+@OptIn(ExperimentalTextApi::class)
+val fontName = GoogleFont("Inter")
+
+@OptIn(ExperimentalTextApi::class)
+val fontFamily = FontFamily(
+    Font(googleFont = GoogleFont("Poppins"), fontProvider = provider),
+    Font(googleFont = fontName, fontProvider = provider, weight = FontWeight.Bold)
+)
+
+
 
 // Set of Material typography styles to start with
+// Here I have added some extra Styles of my own
 val Typography = Typography(
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
-    /* Other default text styles to override
+    ),
+     //Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.W500,
@@ -23,6 +46,27 @@ val Typography = Typography(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp
+    ),
+    h1 = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 35.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.15.sp
+    ),
+    h2 = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 26.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.15.sp
+    ),
+    h3 = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
     )
-    */
+
 )
