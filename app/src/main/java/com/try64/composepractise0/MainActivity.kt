242a5b18d416930158c58671aacb727b1c291de1
Abu Zafar Newton <newton@shurjomukhi.com.bd>
2023-03-26 17:13:37 +0600

    digital clock functionality done.

--- app/src/main/java/com/try64/composepractise0/MainActivity.kt
+++ app/src/main/java/com/try64/composepractise0/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -16,32 +16,68 @@ import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import com.try64.composepractise0.ui.theme.AlarmClockTheme
 import com.try64.composepractise0.ui.theme.NavigationBarShadowColor
+import kotlinx.coroutines.delay
+import java.util.Calendar
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
+            var hour by remember { mutableStateOf("0") }
+            var minute by remember { mutableStateOf("0") }
+            var second by remember { mutableStateOf("0") }
+            var amOrPm by remember { mutableStateOf("0") }
+            val place by remember { mutableStateOf("Dhaka,Bangladesh") }
+
+            LaunchedEffect(Unit){
+                while(true){
+                    val cal = Calendar.getInstance()
+                    hour = cal.get(Calendar.HOUR).run {
+                        if(this.toString().length == 1) "0$this" else "$this"
+                    }
+                    minute = cal.get(Calendar.MINUTE).run {
+                        if(this.toString().length == 1) "0$this" else "$this"
+                    }
+                    second = cal.get(Calendar.SECOND).run {
+                        if(this.toString().length == 1) "0$this" else "$this"
+                    }
+                    amOrPm = cal.get(Calendar.AM_PM).run {
+                        if(this == Calendar.AM) "AM" else "PM"
+                    }
+                    delay(1000)
+                }
+            }
+
             AlarmClockTheme {
                 Scaffold(bottomBar = { NavigationBarComponent() }) {
                     Box(
                         modifier = Modifier
                             .padding(it)
-                            .fillMaxSize()
+                            .fillMaxSize(),
+                        contentAlignment = Alignment.Center
                     ) {
                         Column(horizontalAlignment = Alignment.CenterHorizontally) {
                             HeaderComponent()
                             Box(
                                 modifier = Modifier
                                     .fillMaxSize()
-                                    .fillMaxHeight(fraction = 0.8f)
+                                    .fillMaxHeight(fraction = 0.8f),
+                                contentAlignment = Alignment.Center
                             ) {
                                 Column(horizontalAlignment = Alignment.CenterHorizontally) {
                                     AnalogClockComponent()
                                     Spacer(modifier = Modifier.height(24.dp))
-                                    DigitalCLockComponent()
+                                    DigitalCLockComponent(
+                                        hour = hour,
+                                        minute = minute,
+                                        amOrPm = amOrPm,
+                                        place = place
+                                    )
                                 }
                             }
 
@@ -56,7 +92,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun NavigationBarComponent() {
     BottomNavigation(
-        modifier = Modifier.shadow(ambientColor = NavigationBarShadowColor, elevation = 50.dp, clip = true)
+        modifier = Modifier
+            .shadow(ambientColor = NavigationBarShadowColor, elevation = 50.dp, clip = true)
             .clip(RoundedCornerShape(topStart = 10.dp, topEnd = 10.dp))
     ){
         BottomNavigationItem(
@@ -120,7 +157,22 @@ fun AnalogClockComponent() {
 }
 
 @Composable
-fun DigitalCLockComponent() {
+fun DigitalCLockComponent(
+    hour:String,
+    minute:String,
+    amOrPm:String,
+    place:String
+) {
+    Text(text = "$hour:$minute $amOrPm",
+    style = MaterialTheme.typography.h1)
 
+    Text(text = "$place",
+    style = MaterialTheme.typography.h2.merge(
+        TextStyle(
+            color = MaterialTheme.colors.onBackground.copy(
+                alpha = 0.6f
+            )
+        )
+    ))
 }
 
